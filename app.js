36f43b6d936439b3761ba14055f28aa813699757
boishik <baruaboishik@gmail.com>
2025-08-04 23:18:20 +0600

    API Fetch Flag & Amount

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const BASE_URL =
   "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
 
 const dropdowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("form button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
 
 let i = 0;
 for (let select of dropdowns) {
@@ -29,3 +32,18 @@ const updateFlag = (element) => {
   let img = element.parentElement.querySelector("img");
   img.src = newSrc;
 };
+
+btn.addEventListener("click", async (evt) => {
+  evt.preventDefault();
+  let amount = document.querySelector(".amount input");
+  let amountVal = amount.value;
+  if (amountVal == "" || amountVal < 1) {
+    amountVal = 1;
+    amount.value = "1";
+  }
+
+  //console.log(fromCurr.value, toCurr.value);
+  const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
+  let response = await fetch(URL);
+  console.log(response);
+});
